d4465bf1027a5a3aa9e68353d861b83acf2c666e
Ondrej Necas <ondrej.necas@second-foundation.eu>
2023-09-12 21:55:29 +0200

    PoC for Redis communication

--- services/weather.service/build.gradle.kts
+++ services/weather.service/build.gradle.kts
@@ -1,48 +1,28 @@
-//import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
-//import com.bmuschko.gradle.docker.tasks.image.Dockerfile
-
 plugins {
-    val kotlinVersion = libs.versions.kotlin
-    val kotlinSpringVersion = libs.versions.kotlinSpring
-
+    // JVM
     java
-    kotlin("jvm") version kotlinVersion
     application
-
-    kotlin("plugin.spring") version kotlinSpringVersion
-    id("org.springframework.boot") version "3.1.3"
-	id("io.spring.dependency-management") version "1.1.3"
-
-//    id("com.bmuschko.docker-remote-api") version libs.versions.dockerRemoteApi
-//    id("com.bmuschko.docker-spring-boot-application") version "9.3.2"
-//    id("com.bmuschko.docker-java-application") version libs.versions.dockerJavaApplication
+    kotlin("jvm") version libs.versions.kotlin
+    // Spring
+    kotlin("plugin.spring") version libs.versions.kotlinSpring
+    alias(libs.plugins.springBootPlugin)
+    alias(libs.plugins.springDependencyManagementPlugin)
 }
 
+// JVM plugins specifications
 val javaVersion: Int = libs.versions.java.get().toInt()
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(javaVersion))
     }
 }
-
+val mainClassPath: String = "org.necasond.Main"
+application {
+    mainClass.set(mainClassPath)
+}
 kotlin {
     jvmToolchain(javaVersion)
 }
-
-application {
-    mainClass.set("org.necasond.Main")
-}
-
-//docker {
-//    url = "unix:///var/run/docker.sock"
-//    springBootApplication {
-//        baseImage.set("openjdk:8-alpine")
-//        ports.set(listOf(9090, 8080))
-//        images.set(setOf("awesome-spring-boot:1.115", "awesome-spring-boot:latest"))
-//        jvmArgs.set(listOf("-Dspring.profiles.active=production", "-Xmx2048m"))
-//    }
-//}
 
 repositories {
     mavenCentral()
@@ -60,24 +40,14 @@ dependencies {
     implementation(libs.springBootKotlinStdLib)
     // Kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-
-//    implementation("com.bmuschko:gradle-docker-plugin:9.3.2")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    // Redis
+    implementation("io.github.crackthecodeabhi:kreds:0.9.0")
 }
 
 tasks {
-//    register<Dockerfile>("createDockerfile") {
-//        group = "necasond"
-//        destFile.set(file("build/docker/Dockerfile"))
-//        from("openjdk:17-jdk")
-//        copyFile("/libs/weather.service-standalone.jar", "/app/app.jar")
-//        entryPoint("java")
-//        defaultCommand("-jar", "/app/app.jar")
-//        exposePort(8080)
-//        runCommand("apk --update --no-cache add curl")
-//        instruction("HEALTHCHECK CMD curl -f http://localhost:8080/actuator/health || exit 1")
-//    }
-
     val fatJar = register<Jar>("fatJar") {
         group = "necasond"
         // We need this for Gradle optimization to work
--- services/weather.service/src/main/kotlin/org/necasond/Main.kt
+++ services/weather.service/src/main/kotlin/org/necasond/Main.kt
@@ -1,5 +1,12 @@
 package org.necasond
 
+import io.github.crackthecodeabhi.kreds.connection.Endpoint
+import io.github.crackthecodeabhi.kreds.connection.KredsClientConfig
+import io.github.crackthecodeabhi.kreds.connection.newClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,6 +16,23 @@ class WeatherApplication
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        runApplication<WeatherApplication>(*args)
+        runBlocking {
+            launch(Dispatchers.Default) {
+                val kredClient = newClient(
+                    endpoint = Endpoint.from("10.43.167.81:6379"),
+                ).apply { auth("WF4tUFtQNi") }
+
+                kredClient.use { client ->
+                    client.set("foo", "100")
+                    while (true) {
+                        val num = client.get("foo")
+                        println("foo = $num")
+                        delay(5000)
+                        client.set("foo", (num + 1))
+                    }
+                } // <--- the client/connection to redis is closed.
+            }
+            runApplication<WeatherApplication>(*args)
+        }
     }
 }
